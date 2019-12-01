6f5fd032c545fdf33ec5b7393896abbae9ba49e1
mdashikuzzaman <56515555+mdashikuzzaman@users.noreply.github.com>
2019-12-01 10:53:35 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function f1(){
+		var name = document.getElementById('name');
+		
+		if(name.value == ""){
+
+			var obj = document.getElementById('msg');
+			obj.innerHTML = "null submission";
+		}
+	}
+	
+	
